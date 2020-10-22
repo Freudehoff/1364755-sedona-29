507d2f4dfb27c0337f198e488952e5a7e333de31
Freudehoff <60271500+Freudehoff@users.noreply.github.com>
2020-10-22 23:46:34 +0300

    Поправил анимацию

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ const orderButton = document.querySelector(".order-button");
 const search = document.querySelector(".search");
 const searchForm = search.querySelector(".search-form");
 const dateIn = search.querySelector("[name=datein]");
-const dateOut =  search.querySelector("[name=dateout]");
-const adult =  search.querySelector("[name=adult]");
-const children =  search.querySelector("[name=children]");
+const dateOut = search.querySelector("[name=dateout]");
+const adult = search.querySelector("[name=adult]");
+const children = search.querySelector("[name=children]");
 
 orderButton.addEventListener("click", function (evt) {
   evt.preventDefault();
-    search.classList.toggle("search-hide");
+  search.classList.toggle("search-hide");
 });
 
 searchForm.addEventListener("submit", function (evt) {
